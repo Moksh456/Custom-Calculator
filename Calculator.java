d2402a70d8217599a4fab34790be23abddcb729c
Moxh <moxhsharma878@gmail.com>
2025-04-03 00:34:29 +0530

    adding some basic code Structure of Calculator

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+class custormcal{
+    double add(double a, double b){
+return a+b;
+    }
+    double sustract(double a, double b){
+        return a-b;
+            }
+            double divide(double a, double b){
+                return a/b;
+                    }
+                    double multiply(double a, double b){
+                        return a*b;
+                            }
+                    
+}
+public class Calculator{
+    public static void main(String[] args) {
+        //this is quetions throws expections 
+    }
+}
